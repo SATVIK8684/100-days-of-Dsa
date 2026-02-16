4cb6a785277b6726cd0e2aa9536e354d75e1185b
SATVIK8684 <satvik4848@gmail.com>
2026-02-16 14:57:44 +0530

    Add files via upload

---
+++ D2Q1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Input size of array
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int pos;
+    // Input position to delete (1-based index)
+    scanf("%d", &pos);
+
+    // Check if position is valid
+    if(pos < 1 || pos > n) {
+        printf("Invalid position");
+        return 0;
+    }
+
+    // Shift elements to the left
+    for(int i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    // Print updated array (size becomes n-1)
+    for(int i = 0; i < n - 1; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
